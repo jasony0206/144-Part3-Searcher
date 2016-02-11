0e44f38beb6dfe4030d2e35baa795a28305e2eb1
tincan24 <tincan24@gmail.com>
2016-02-10 21:26:16 -0800

    Run all AuctionSearchTest and removed .DS_Store

--- src/edu/ucla/cs/cs144/AuctionSearchTest.java
+++ src/edu/ucla/cs/cs144/AuctionSearchTest.java
@@ -16,22 +16,22 @@ public class AuctionSearchTest {
 		String reply = as.echo(message);
 		System.out.println("Reply: " + reply);
 
-//		String query = "star trek";
-//		SearchResult[] basicResults = as.basicSearch(query, 0, 800);
-//		System.out.println("Basic Seacrh Query: " + query);
-//		System.out.println("Received " + basicResults.length + " results");
-//		for(SearchResult result : basicResults) {
-//			System.out.println(result.getItemId() + ": " + result.getName());
-//		}
+		String query = "star trek";
+		SearchResult[] basicResults = as.basicSearch(query, 0, 800);
+		System.out.println("Basic Seacrh Query: " + query);
+		System.out.println("Received " + basicResults.length + " results");
+		for(SearchResult result : basicResults) {
+			System.out.println(result.getItemId() + ": " + result.getName());
+		}
 		
-//		SearchRegion region =
-//		    new SearchRegion(33.774, -118.63, 34.201, -117.38);
-//		SearchResult[] spatialResults = as.spatialSearch("camera", region, 0, 20);
-//		System.out.println("Spatial Seacrh");
-//		System.out.println("Received " + spatialResults.length + " results");
-//		for(SearchResult result : spatialResults) {
-//			System.out.println(result.getItemId() + ": " + result.getName());
-//		}
+		SearchRegion region =
+		    new SearchRegion(33.774, -118.63, 34.201, -117.38);
+		SearchResult[] spatialResults = as.spatialSearch("camera", region, 0, 20);
+		System.out.println("Spatial Seacrh");
+		System.out.println("Received " + spatialResults.length + " results");
+		for(SearchResult result : spatialResults) {
+			System.out.println(result.getItemId() + ": " + result.getName());
+		}
 
 		String itemId = "1049471876";
 		String item = as.getXMLDataForItemId(itemId);
